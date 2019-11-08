710aadd9b95194992890feb9786f3e831802e3fd
mmalecki <malecki.mateusz12@gmail.com>
2019-11-08 21:52:00 +0100

    Fixed linting

--- Frontend/src/Types.elm
+++ Frontend/src/Types.elm
@@ -1,49 +1,55 @@
 module Types exposing (..)
 
 import Random
-import Time
 import Task
+import Time
 
-type alias Record = {
-    url : String,
-    title : String
+
+type alias Record =
+    { url : String
+    , title : String
     }
+
 
 sampleRecord : Record
 sampleRecord =
     Record "https://guide.elm-lang.org/types/type_aliases.html" "Custom Title"
 
-type alias Category = {
-    name : String
+
+type alias Category =
+    { name : String
     , tags : List String
     , records : List Record
     }
 
+
 sampleCategory : Category
-sampleCategory = 
-    Category "Sample name" (List.repeat 2 "Tag") (List.repeat 5 sampleRecord) 
+sampleCategory =
+    Category "Sample name" (List.repeat 2 "Tag") (List.repeat 5 sampleRecord)
 
 
-
-type Item = 
-    Section Category 
-    | CustomClock Clock 
+type Item
+    = Section Category
+    | CustomClock Clock
 
 
 sampleItemCategory : Item
-sampleItemCategory = Section sampleCategory
+sampleItemCategory =
+    Section sampleCategory
+
 
 sampleClock : Clock
 sampleClock =
     Clock "Polska" Time.utc (Time.millisToPosix 0)
 
+
 sampleItemClock : Item
-sampleItemClock = CustomClock sampleClock
+sampleItemClock =
+    CustomClock sampleClock
 
 
-type alias Clock = {
-    title : String
+type alias Clock =
+    { title : String
     , zone : Time.Zone
     , time : Time.Posix
     }
-
