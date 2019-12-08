ee069700fd3af259b7e25ca130a4d61ccb58b456
inql <dbinkus96@gmail.com>
2019-12-08 14:55:59 +0100

    Prework for routes unit testing

---
+++ src/test/scala/core/RoutesSpec.scala
@@ -0,0 +1,11 @@
+package core
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{Matchers, WordSpec}
+
+class RoutesSpec extends WordSpec with Matchers with ScalatestRouteTest {
+
+  val route:
+
+
+}
