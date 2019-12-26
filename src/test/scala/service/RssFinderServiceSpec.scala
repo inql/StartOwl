3800a3bfd3b027183b5e43443f3927037ebe8652
inql <dbinkus96@gmail.com>
2019-12-26 01:18:26 +0100

    Add additional assertions for unit tests

--- src/test/scala/service/RssFinderServiceSpec.scala
+++ src/test/scala/service/RssFinderServiceSpec.scala
@@ -1,5 +1,6 @@
 package service
 
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
 
 class RssFinderServiceSpec extends WordSpec with Matchers with BeforeAndAfterEach{
@@ -9,6 +10,7 @@ class RssFinderServiceSpec extends WordSpec with Matchers with BeforeAndAfterEac
   val urlToCheckApplicationRssFeeds = "https://tvn24.pl"
   val urlToCheckBloggerFeeds = "https://nikonorov.pl"
   val urlToCheckCombinedFeeds = "https://wykop.pl"
+  val browser = JsoupBrowser()
 
 
   var wordPressRssFinderService: RssFinderService = _
@@ -30,52 +32,70 @@ class RssFinderServiceSpec extends WordSpec with Matchers with BeforeAndAfterEac
     "return a String when wordPressFeed function is called" in {
       val result = wordPressRssFinderService.wordPressFeed
       result shouldBe a [String]
+      noException should be thrownBy browser.get(result)
     }
 
     "return a String when tumblrFeed function is called" in {
       val result = tumblrRssFinderService.tumblrFeed
       result shouldBe a [String]
+      noException should be thrownBy browser.get(result)
     }
 
     "return a list of Strings when rssSourceUrl function is called" in {
       val result = applicationRssFinderService.rssSourceUrl
       result shouldBe a [List[String]]
+      result.foreach(
+        noException should be thrownBy browser.get(_)
+      )
     }
 
     "return a String when bloggerFeed function is called" in {
       val result = bloggerRssFinderService.wordPressFeed
       result shouldBe a [String]
+      noException should be thrownBy browser.get(result)
     }
 
     "return valid URL (not \"\") when wordPressFeed function is called" in {
       val result = wordPressRssFinderService.wordPressFeed
       result should not be ""
+      noException should be thrownBy browser.get(result)
     }
 
     "return valid URL (not \"\") when tumblrFeed function is called" in {
       val result = tumblrRssFinderService.tumblrFeed
       result should not be ""
+      noException should be thrownBy browser.get(result)
     }
 
     "return valid URL (not \"\") when rssSourceUrl function is called" in {
       val result = applicationRssFinderService.rssSourceUrl
       result should not contain ""
+      result.foreach(
+        noException should be thrownBy browser.get(_)
+      )
     }
 
     "return valid URL (not \"\") when bloggerFeed function is called" in {
       val result = bloggerRssFinderService.wordPressFeed
       result should not be ""
+      noException should be thrownBy browser.get(result)
     }
 
     "return a list of Strings when getAllValidRssFeeds is called" in {
       val result = combinedRssFinderService.getAllValidRssFeeds()
       result shouldBe a [List[String]]
+      result.foreach(
+        noException should be thrownBy browser.get(_)
+      )
     }
 
     "return all valid URLs (list not contains \"\") when getAllValidRssFeeds is called" in {
       val result = combinedRssFinderService.getAllValidRssFeeds()
       result should not contain ""
       result should not be empty
+      result.foreach(
+        noException should be thrownBy browser.get(_)
+      )
     }
 
   }
