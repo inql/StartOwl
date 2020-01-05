94b3875c9ecdc7ce2562f1094a12a70749752d06
mmalecki <malecki.mateusz12@gmail.com>
2020-01-05 17:27:40 +0100

    Fixed clocks display

--- Frontend/src/Forms/ClockForm.elm
+++ Frontend/src/Forms/ClockForm.elm
@@ -6,6 +6,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, onClick, onInput)
 import List.Selection exposing (Selection)
+import SiteItems.Clocks exposing (..)
 import Time
 import TimeZone exposing (..)
 
@@ -13,18 +14,19 @@ import TimeZone exposing (..)
 type alias Model =
     { name : String
     , zone : Time.Zone
+    , clock : Maybe Clock
     }
 
 
 type Msg
-    = UpdateName String
-    | UpdateTimeZone String
+    = UpdateTimeZone String
     | SubmitForm
+    | ClockMsg SiteItems.Clocks.Msg
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model "" defaultTimeZone
+    ( Model "New clock" defaultTimeZone Nothing
     , Cmd.none
     )
 
@@ -36,31 +38,38 @@ defaultTimeZone =
 
 possibleTimeZones : Dict String Time.Zone
 possibleTimeZones =
-    [ ( "Londyn", europe__london () ), ( "Warszawa", europe__warsaw () ), ( "Nowy York", america__new_york () ), ( "Tokio", asia__tokyo () ) ]
+    [ ( "", europe__warsaw () ), ( "Londyn", europe__london () ), ( "Warszawa", europe__warsaw () ), ( "Nowy York", america__new_york () ), ( "Tokio", asia__tokyo () ) ]
         |> List.sortBy Tuple.first
         |> Dict.fromList
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    case model.clock of
+        Nothing ->
+            Sub.none
+
+        Just c ->
+            Sub.map ClockMsg (SiteItems.Clocks.subscriptions c)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg, Maybe Model )
 update msg model =
     case msg of
-        UpdateName newName ->
-            ( { model | name = newName }, Cmd.none, Nothing )
-
         UpdateTimeZone keyNewZone ->
-            ( { model
-                | zone =
+            let
+                newZone =
                     case Dict.get keyNewZone possibleTimeZones of
                         Just val ->
                             val
 
                         Nothing ->
                             defaultTimeZone
+            in
+            ( { model
+                | zone = newZone
+                , name = keyNewZone
+                , clock = Just (Clock -1 keyNewZone newZone (Time.millisToPosix 0))
               }
             , Cmd.none
             , Nothing
@@ -74,6 +83,17 @@ update msg model =
                 _ ->
                     ( model, Cmd.none, Nothing )
 
+        ClockMsg m ->
+            case model.clock of
+                Just cl ->
+                    ( { model | clock = Just (Tuple.first (SiteItems.Clocks.update m cl)) }
+                    , Cmd.none
+                    , Nothing
+                    )
+
+                Nothing ->
+                    ( model, Cmd.none, Nothing )
+
 
 view : Model -> Html Msg
 view model =
@@ -83,15 +103,24 @@ view model =
 displayForm : Model -> Html Msg
 displayForm model =
     div []
-        [ input [ placeholder "name", value model.name, onInput UpdateName ] []
-        , br [] []
-        , br [] []
+        [ h1 [] [ text model.name ]
+        , displayClock model.clock
         , select [ onInput UpdateTimeZone ]
             (possibleTimeZones |> Dict.toList |> List.map (\x -> Tuple.first x) |> List.map (\x -> option [ value x ] [ text x ]))
         , Button.button
             [ Button.primary, Button.attrs [ onClick SubmitForm ] ]
             [ text "Submit" ]
         ]
+
+
+displayClock : Maybe Clock -> Html Msg
+displayClock mclock =
+    case mclock of
+        Just clock ->
+            Html.map ClockMsg (SiteItems.Clocks.displayClock clock)
+
+        Nothing ->
+            h2 [] [ text "--:--:--" ]
 
 
 validateNewClock : Model -> Bool
